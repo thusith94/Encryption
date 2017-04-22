cc2e083afd25ab7f9b9e97419ba2c426ec0d6329
Thusitha Kumarage <tkumarage20@gmail.com>
2017-04-22 19:20:10 +0530

    Add files via upload

---
+++ src/Symmetric.java
@@ -0,0 +1,38 @@
+
+import MainWindow.Interface;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thusitha Dilhan
+ */
+public class Symmetric {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface in=new Interface();
+        in.setVisible(true);
+        
+    }
+    
+    
+}
